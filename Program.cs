495c1343c5683a9486281706da31e371e128d9de
João Bosco Dos Reis Becker <joao.jbosco@gmail.com>
2024-01-28 18:20:51 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace ConsoleNullable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Hello World!");
+                //double x = null;  Não é possível realizar essa declaração
+                Nullable<double> x = null; ///Permitido
+                double? y = null;          ///Permitido      
+                y = 10.0;
+                Console.WriteLine("x é null" + x);
+                Console.WriteLine("y é nullable" + y);
+                Console.WriteLine(x.GetValueOrDefault());
+                Console.WriteLine(y.GetValueOrDefault());
+                Console.WriteLine(x.HasValue); // Property HasValue retorna True ou False caso tenha valor
+                Console.WriteLine(y.HasValue);// Property HasValue retorna True ou False caso tenha valor
+                if (x.HasValue)
+                    Console.WriteLine(x.Value);
+                else Console.WriteLine("X é Null");
+
+                if (y.HasValue)
+                    Console.WriteLine(y.Value);
+                else
+                    Console.WriteLine("Y é Null");
+
+                // Operador de coalescência null
+                y = x ?? 100;
+
+                Console.WriteLine(y.Value); // No InvalidOperationException
+            }
+            catch (Exception e)
+            {
+                throw new ArgumentNullException(e.Message + e.Source, message: "Name cannot be null");
+            }
+        }
+    }
+}
